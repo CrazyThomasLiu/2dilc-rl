bc03460bf047a4c1279f59d852ce2a4f767bab49
Jianan Liu <liujianan19930909@hotmail.com>
2023-09-06 21:40:23 +0800

    Revision

--- Supplementary_Material/figure13.py
+++ Supplementary_Material/figure13.py
@@ -36,7 +36,21 @@ with f_ref:
         ilc[num]=row['Value']
         num+=1
 
+'get the PI-based indirect-type ILC '
+figure_data_path=os.path.join(current_dir, "Figure13_original3.csv")
+'get the length of the data'
+f_ref=open(figure_data_path,'r')
+length=len(f_ref.readlines())-1
+num=0
+pi_ilc=np.zeros(length)
+f_ref=open(figure_data_path,'r')
+with f_ref:
+    reader=csv.DictReader(f_ref)
+    for row in reader:
+        pi_ilc[num]=row['Value']
+        num+=1
+
 with open('Figure13.csv', 'w', encoding='UTF8', newline='') as f:
     writer = csv.writer(f)
-    writer.writerow(['2D ILC-RL Control Scheme','2D Iterative Learning Control Scheme'],)
-    writer.writerows(map(lambda x,y: [x,y], ilc_rl,ilc))
+    writer.writerow(['2D ILC-RL Control Scheme','2D Iterative Learning Control Scheme','PI-based Indirect ILC'],)
+    writer.writerows(map(lambda x,y,z: [x,y,z], ilc_rl,ilc,pi_ilc))
